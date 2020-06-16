6944ad4b82a030cb7a5fabf3dc34c39278b1e1de
c-noblet <c.noblet.dev@gmail.com>
2020-06-16 16:51:04 +0200

    init connect route

---
+++ api/routes/connect.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { client, silenceJob, discordPlayer } = require('../../discord/index.js');
+
+// Connect the bot to the channel
+router.get('/:id', async (req, res) => {
+  try {
+    const channel = client.channels.cache.get(req.params.id);
+    const connection = await channel.join();
+    discordPlayer.setConnection(connection);
+    silenceJob.start();
+    connection.on('disconnect', silenceJob.stop());
+    res.status(200).json({ message: 'connecting' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
