5f8c1aebadcb162f341e72734a4db915aec7425c
c-noblet <c.noblet.dev@gmail.com>
2020-09-29 22:48:02 +0200

    update to serve https

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
+const privateKey = fs.readFileSync('./ssl/server.key');
+const certificate = fs.readFileSync('./ssl/server.crt');
+
+const credentials = {key: privateKey, cert: certificate};
+
 const app = require('./api/index.js');
 const { client } = require('./discord/index.js');
 
-app.listen(process.env.PORT, () => console.log('server started'));
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(process.env.PORT);
 client.login(process.env.TOKEN);
